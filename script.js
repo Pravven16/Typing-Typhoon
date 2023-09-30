4391e8d90f34ac7279db1f36c71ad74b01364fb6
pravven <pravven16@gmail.com>
2023-09-30 18:11:11 +0530

    Add JavaScript code to calculate WPM

--- script.js
+++ script.js
@@ -1,46 +1,62 @@
-const typingDiv = document.getElementById("typing")
+const typingDiv = document.getElementById("typing");
+const statsDiv = document.getElementById("stats");
+const startGameBtn = document.getElementById("start-game");
+const paragraphs = [
+    `A well-organized paragraph`,
+    `The band invents a new way to practice via four-way iChat. To the dismay of his bandmates, Fish performs in the nude due to the heat of his new residence. The video of the practice quickly goes viral under the title of the "naked drummer band". The band is signed to a recording contract by the same label as Vesuvius, and are sent on a midwest tour. However, Fish commits stereotypical acts, despite the physical costs on his body, and he vandalizes a hotel room, causing the band to be apprehended again`
+];
 
-console.log(typingDiv);
+const startGame = () => {
+    startGameBtn.classList.add('hidden');
+    typingDiv.innerHTML = "";
+    statsDiv.innerHTML = "";
 
-const text = `A well-organized paragraph`
+    const text = paragraphs[Math.floor(Math.random() * paragraphs.length)];
 
-const characters = text.split("").map((char) => {
-    const span = document.createElement("span")
-    span.innerText = char
-    typingDiv.appendChild(span)
-    return span
-});
+    const characters = text.split("").map((char) => {
+        const span = document.createElement("span");
+        span.innerText = char;
+        typingDiv.appendChild(span);
+        return span;
+    });
 
-let cursorIndex = 0;
-let startTime = null;
-let endDate = null;
-let cursorCharacter = characters[0];
-cursorCharacter.classList.add("cursor");
+    let cursorIndex = 0;
+    let startTime = null;
+    let endTime = null;
+    let cursorCharacter = characters[cursorIndex];
+    cursorCharacter.classList.add("cursor");
 
-const keyListener = document.addEventListener("keydown", ({ key }) =>{
-    if(!startTime) {
-    startTime = new Date();
-    }
-    if(key === cursorCharacter.innerText) {
-        // we typed the correct key
-        cursorCharacter.classList.remove("cursor");
-        cursorCharacter.classList.add("done");
-        // characters[cursorIndex + 1].classList.add("cursor");
-        cursorCharacter = characters[++cursorIndex];
-    }
-    if (cursorIndex >= characters.length) {
-        endTime = new Date();
-        const delta = endTime - startTime;
-        const seconds = delta / 1000;
-        const numberOfWords = text.split(" ").length;
-        const wps = numberOfWords / seconds;
-        const wpm = wps * 60;
-        document.getElementById('stats').innerText = `wpm = $(wpm)`
-        // display the wpm, cpm
-        document.removeEventListener("keydown",keyListener);
+    const keydown = ({ key }) => {
+        if (!startTime) {
+            startTime = new Date();
+        }
+        if (key === cursorCharacter.innerText) {
+            // we typed the correct key
+            cursorCharacter.classList.remove("cursor");
+            cursorCharacter.classList.add("done");
+            cursorCharacter = characters[++cursorIndex];
+        }
 
-        return;
-    }
+        if (cursorIndex >= characters.length) {
+            // game ended
+            endTime = new Date();
+            const delta = endTime - startTime;
+            const seconds = delta / 1000;
+            const numberOfWords = text.split(" ").length;
+            const wps = numberOfWords / seconds;
+            const wpm = wps * 60;
+            document.getElementById('stats').innerText = `wpm = ${wpm}`;
+
+            // display the wpm, cpm
+            document.removeEventListener("keydown", keydown);
+            startGameBtn.classList.remove('hidden');
+            return;
+        }
         cursorCharacter.classList.add("cursor");
-    
-});
+    };
+
+    document.addEventListener("keydown", keydown);
+};
+
+// Add an event listener to the "Start Game" button
+startGameBtn.addEventListener("click", startGame);
